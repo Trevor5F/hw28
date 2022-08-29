cac83ffc593fd9904da433c7678b284bd24517e1
Trevor5F <101113379+Trevor5F@users.noreply.github.com>
2022-08-29 15:43:38 +0300

    Delete hw_29 directory

--- hw_29/ads/views/locations.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-
-from ads.models import Location
-from ads.serializers import LocationSerializer
-
-
-class LocationViewSet(ModelViewSet):
-    queryset = Location.objects.all()
-    serializer_class = LocationSerializer
